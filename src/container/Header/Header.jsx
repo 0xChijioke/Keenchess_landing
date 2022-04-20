c56ef0306d857053c8bad21d5f89ec0235604462
chijoke ogbuanya <chijokeogbuanya@gmail.com>
2022-04-20 18:02:16 +0100

    UI works

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => (
     <div className="app__wrapper_info">
       <h1 className="app__header-h1">Chess For An Improved Life</h1>
       <p className="typing__letter  p__opensans" style={{ margin: '2rem 0' }}> Learn and earn real cash!</p>
-      {/* <button type="button" className="custom__button">Explore Menu</button> */}
+      <button type="button" className="custom__button">Register Now!</button>
     </div>
 
     <div className="app__wrapper_img">

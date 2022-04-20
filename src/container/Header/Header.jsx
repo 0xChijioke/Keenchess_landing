d3183c5584ef28a53b89fe0dd8f202567935df81
chijoke ogbuanya <chijokeogbuanya@gmail.com>
2022-04-20 19:07:14 +0100

    homework

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,9 +5,17 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <h1 className="app__header-h1">Chess For An Improved Life</h1>
+      <h1 className="app__header-h1">chess for an improved life</h1>
       <p className="typing__letter  p__opensans" style={{ margin: '2rem 0' }}> Learn and earn real cash!</p>
-      <button type="button" className="custom__button">Register Now!</button>
+      <button type="button" className="animate__btn">
+        <a href="#login">
+          <span />
+          <span />
+          <span />
+          <span />
+          Register Now!
+        </a>
+      </button>
     </div>
 
     <div className="app__wrapper_img">

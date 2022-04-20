d333a2679735262a3281c849d653d0a41c926c18
chijoke ogbuanya <chijokeogbuanya@gmail.com>
2022-04-20 20:15:25 +0100

    user interfacing

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <h1 className="app__header-h1">chess for an improved life</h1>
-      <p className="typing__letter  p__opensans" style={{ margin: '2rem 0' }}>we reward your chess knowleage!</p>
-      <button type="button" className="animate__btn">
+      <p className="typing__letter  p__opensans" style={{ margin: '2rem 0' }}>play and earn rewards!</p>
+      <button type="button" onClick={{ className: 'animate__btn a:click' }} className="animate__btn">
         <a href="#login">
           <span />
           <span />

5bdf7abdc8bd0bd8b8d628e3cbafefa6e21e4a96
chijoke ogbuanya <chijokeogbuanya@gmail.com>
2022-04-20 16:17:38 +0100

    fix responsivness

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <h1 className="app__header-h1">Chess For An Improved Life</h1>
-      <p className="typing__letter  p__opensans" style={{ margin: '2rem 0' }}> Learn and earn real cash from your CHESS PASSION!</p>
+      <p className="typing__letter  p__opensans" style={{ margin: '2rem 0' }}> Learn and earn real cash!</p>
       {/* <button type="button" className="custom__button">Explore Menu</button> */}
     </div>
 
